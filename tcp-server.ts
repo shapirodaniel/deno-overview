39e47883ef5abf54dda975750470d0958d34a8f7
Daniel Shapiro <daniel.shapiro@avanade.com>
2021-09-11 13:38:03 -0500

    fix: comments in tcp-server demo

--- tcp-server.ts
+++ tcp-server.ts
@@ -1,3 +1,4 @@
+// will fail without --allow-net
 import { copy } from "https://deno.land/std@0.106.0/io/util.ts";
 const hostname = "0.0.0.0";
 const port = 8080;
@@ -7,4 +8,3 @@ for await (const conn of listener) {
   console.log("hello world -- someone pinged me");
   copy(conn, conn);
 }
-// will fail without --allow-net
